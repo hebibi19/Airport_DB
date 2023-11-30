d01e8074564e6320a45a5fe414ad4c1e977a86d6
Jacqueline Molina <72059293+hebibi19@users.noreply.github.com>
2023-11-30 15:35:55 -0800

    Added Remove Airplane, Remove Test, displayAll

--- airport/Main.java
+++ airport/Main.java
@@ -2,17 +2,26 @@ package airport;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
 	
-	// i literally cannot figure out how to connect to the database im :sobbing:
+	// change this to your info
+	static final String DB_URL = "jdbc:postgresql://cs1.calstatela.edu/cs4222f23hp39"; // your user here
+	static final String USER = "cs4222f23hp39"; // your user here
+	static final String PASS = "rW2MPLBR"; // your password here
 	
-	static final String DB_URL = "jdbc:postgresql://cs1.calstatela.edu:5432/";
-	static final String USER = "---";
-	static final String PASS = "---";
+	
+	static final String DisplayAirplanes = "SELECT * FROM Airplane";
+	static final String DisplayTests = "SELECT * FROM Test";
+	static final String DisplayModels = "SELECT * FROM Model";
+	static final String DisplayTraffic = "SELECT * FROM Traffic_Controller";
+	static final String DisplayTechnicians = "SELECT * FROM Technician";
+	static final String DisplayExperts = "SELECT * FROM Expert_On";
 
 	public static void main(String[] args) {
 		
@@ -25,7 +34,6 @@ public class Main {
 			// calling the initial menu
 			initialMenu(stmt);
 
-			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -37,7 +45,55 @@ public class Main {
 		
 	}
 	
+	/**
+	 * Method removes chosen Airplane from database
+	 */
 	public static void removePlane(Statement stmt) {
+		// stores the primary key so deletion is easier to accomplish
+		HashMap<Integer, String> h = new HashMap<>();
+		
+		try {
+			// display airplanes
+			ResultSet rs = stmt.executeQuery(DisplayAirplanes);
+			
+			int count = 1;
+			// display airplanes + add primary key to hash map
+			while (rs.next()) {
+				System.out.print("\n[" + count + "]");
+				System.out.println("\tRegistration Number: " + rs.getString("registration_no"));
+				System.out.println("\tModel Number: " + rs.getString("model_no"));
+				h.put(count, rs.getString("registration_no"));
+				count++;
+
+			}
+			// asking which airplane they want to remove (input a number)
+			System.out.println("\n Which AIRPLANE would you like to remove?");
+			Scanner input = new Scanner(System.in);
+			// deletion choice
+			int choice = input.nextInt();
+			
+			// delete chosen plane
+			String deletePlane = "DELETE FROM Airplane WHERE registration_no = '" + h.get(choice) + "'";
+			stmt.executeUpdate(deletePlane);
+			
+			// display new result set of airplanes
+			ResultSet rs2 = stmt.executeQuery(DisplayAirplanes);
+			
+			count = 1;
+			
+			while (rs2.next()) {
+				System.out.print("\n[" + count + "]");
+				System.out.println("\tRegistration Number: " + rs2.getString("registration_no"));
+				System.out.println("\tModel Number: " + rs2.getString("model_no"));
+				count++;
+			}
+			
+			// call initial menu again
+			initialMenu(stmt);
+				
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
@@ -46,10 +102,166 @@ public class Main {
 	}
 	
 	public static void removeTest(Statement stmt) {
+		// stores the primary key so deletion is easier to accomplish
+		HashMap<Integer, String[]> h = new HashMap<>();
+		
+		try {
+			// display tests
+			ResultSet rs = stmt.executeQuery(DisplayTests);
+			
+			int count = 1;
+			// display tests + add primary keys (2 of them) to hash map
+			while (rs.next()) {
+				System.out.print("\n[" + count + "]");
+				System.out.println("\tRegistration Number: " + rs.getString("registration_no"));
+				System.out.println("\tSSN: " + rs.getString("ssn"));
+				h.put(count, new String[] {rs.getString("registration_no"), rs.getString("ssn")});
+				System.out.println("\tDate of Test: " + rs.getDate("test_date"));
+				System.out.println("\tHours: " + rs.getInt("hours"));
+				System.out.println("\tScore: " + rs.getInt("score"));
+
+				count++;
+
+			}
+			// asking which test they want to remove (input a number)
+			System.out.println("\n Which TEST would you like to remove?");
+			Scanner input = new Scanner(System.in);
+			// deletion choice
+			int choice = input.nextInt();
+			
+			// delete chosen test
+			String deletePlane = "DELETE FROM Test WHERE registration_no = '" + h.get(choice)[0] + "' AND ssn = '" + h.get(choice)[1] + "'";
+			stmt.executeUpdate(deletePlane);
+			
+			// display new result set of test
+			ResultSet rs2 = stmt.executeQuery(DisplayTests);
+			
+			count = 1;
+			
+			while (rs2.next()) {
+				System.out.print("\n[" + count + "]");
+				System.out.println("\tRegistration Number: " + rs2.getString("registration_no"));
+				System.out.println("\tSSN: " + rs2.getString("ssn"));
+				System.out.println("\tDate of Test: " + rs2.getDate("test_date"));
+				System.out.println("\tHours: " + rs2.getInt("hours"));
+				System.out.println("\tScore: " + rs2.getInt("score"));
+
+				count++;
+			}
+			
+			// call initial menu again
+			initialMenu(stmt);
+				
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
 	public static void displayAll(Statement stmt) {
+		try {
+			// display Airplanes
+			ResultSet rs = stmt.executeQuery(DisplayAirplanes);
+			
+			int count = 1;
+			
+			System.out.println("------------ AIRPLANE TABLE -----------");
+			while (rs.next()) {
+				System.out.print("\n[" + count + "]");
+				System.out.println("\tRegistration Number: " + rs.getString("registration_no"));
+				System.out.println("\tModel Number: " + rs.getString("model_no"));
+				count++;
+			}
+			System.out.println("----------------------------------------");
+
+			
+			// display Models
+			ResultSet rs2 = stmt.executeQuery(DisplayModels);
+			
+			count = 1;
+			
+			System.out.println("\n\n------------ MODEL TABLE -----------");
+			while (rs2.next()) {
+				System.out.print("\n[" + count + "]");
+				System.out.println("\tModel Number: " + rs2.getString("model_no"));
+				System.out.println("\tCapacity: " + rs2.getInt("capacity"));
+				System.out.println("\tWeight: " + rs2.getInt("weight"));
+				count++;
+			}
+			System.out.println("----------------------------------------");
+
+			// display Traffic Controllers
+			ResultSet rs3 = stmt.executeQuery(DisplayTraffic);
+			
+			count = 1;
+			
+			System.out.println("\n\n------------ TRAFFIC CONTROLLER TABLE -----------");
+			while (rs3.next()) {
+				System.out.print("\n[" + count + "]");
+				System.out.println("\tSSN: " + rs3.getString("ssn"));
+				System.out.println("\tUnion ID: " + rs3.getInt("union_id"));
+				System.out.println("\tLast Exam Date: " + rs3.getDate("last_exam"));
+				count++;
+			}
+			System.out.println("-------------------------------------------------");
+			
+			// display Traffic Controllers
+			ResultSet rs4 = stmt.executeQuery(DisplayTechnicians);
+			
+			count = 1;
+			
+			System.out.println("\n\n---------------- TECHNICIAN TABLE ------------------");
+			while (rs4.next()) {
+				System.out.print("\n[" + count + "]");
+				System.out.println("\tSSN: " + rs4.getString("ssn"));
+				System.out.println("\tUnion ID: " + rs4.getInt("union_id"));
+				System.out.println("\tFirst Name: " + rs4.getString("fname"));
+				System.out.println("\tLast Name: " + rs4.getString("lname"));
+				System.out.println("\tAddress: " + rs4.getString("address"));
+				System.out.println("\tPhone: " + rs4.getString("phone"));
+				System.out.println("\tSalary: " + rs4.getString("salary"));
+				count++;
+			}
+			System.out.println("-------------------------------------------------");
+			
+			// display Traffic Controllers
+			ResultSet rs5 = stmt.executeQuery(DisplayExperts);
+			
+			count = 1;
+			
+			System.out.println("\n\n------------- EXPERT TABLE ------------");
+			while (rs5.next()) {
+				System.out.print("\n[" + count + "]");
+				System.out.println("\tSSN: " + rs5.getString("ssn"));
+				System.out.println("\tModel Number: " + rs5.getString("model_no"));
+				count++;
+			}
+			System.out.println("---------------------------------------");
+			
+			// display Traffic Controllers
+			ResultSet rs6 = stmt.executeQuery(DisplayTests);
+			
+			count = 1;
+			
+			System.out.println("\n\n---------------- TEST TABLE --------------------");
+			while (rs6.next()) {
+				System.out.print("\n[" + count + "]");
+				System.out.println("\tRegistration Number: " + rs6.getString("registration_no"));
+				System.out.println("\tSSN: " + rs6.getString("ssn"));
+				System.out.println("\tTest Date: " + rs6.getDate("test_date"));
+				System.out.println("\tHours: " + rs6.getInt("hours"));
+				System.out.println("\tScore: " + rs6.getInt("score"));
+				count++;
+			}
+			System.out.println("-------------------------------------------------");
+
+			
+			// call initial menu again
+			initialMenu(stmt);
+				
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
@@ -102,11 +314,6 @@ public class Main {
 				System.exit(0);
 			}
 		}
-		
-
-
-
-
 		
 	}
 
